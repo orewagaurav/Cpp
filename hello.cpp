520d2f6b5f4fc5fcc0a686f6dfe521a351ddd009
Gaurav Kumar <gauravkumar47967@gmail.com>
2025-01-03 00:18:58 +0530

    hello

--- hello.cpp
+++ hello.cpp
@@ -2,4 +2,8 @@
 
 int main(){
     std::cout << "Hello World!\n";
+    int a,b;
+    std::cout<<"Enter the value of two numbers:\n";
+    std::cin >> a >>b;
+    std::cout <<"Sum of given two numbers is: "<<a+b<<"\n";
 }
